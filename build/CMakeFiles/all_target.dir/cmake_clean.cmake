73bfa26b54f778c8cfea1093eda89a3fb570f947
Merok23 <alejocalizblanco@gmail.com>
2023-05-18 13:41:38 -0300

    Se agrego al git ignore lo que buildea cmake .. cosa que se pueda buildear en cualquier maquina sin tener que borrar el cmakecache.txt Se modifico el CMakeList.txt para poder linkear los server_src con los test sin el main. Ahora el main de client y de server tiene que estar en la carpeta main Listos para hacer tests!

--- build/CMakeFiles/all_target.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/all_target"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/all_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
