0860aa05f4e54ab353e80e4bd1f7195652fea799
Pandamos <abrildiazmiguez99@gmail.com>
2023-06-11 15:31:07 -0300

    Codigo original de client_client que levanta todos los datos por qt
    No tiene en cuenta el caso donde el cliente se va del juego sin haber creado nada

--- client/client_client.cpp
+++ client/client_client.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <utility>
 
-
 #include "client_client.h"
 #define MAX_ELEMENTS 10000
 
@@ -16,6 +15,41 @@ Client::Client(const char* hostname, const char* servname) :
     receive_thread = new ReceiveThread(protocol, game_states);
 }
 
+
+//  std::string line;
+//     COMMANDS commands;
+//     command_t create_or_join_command;
+//     command_t player_command;
+
+//     //los inicializo con otro comando, para no leer basura
+//     create_or_join_command = commands.cheatKillAllInfected();
+//     player_command = commands.cheatKillAllInfected();
+
+//     this->graphics_qt.run(&commands, &create_or_join_command, &player_command);
+
+//     if ((create_or_join_command.type == CREATE_ROOM || create_or_join_command.type == JOIN_ROOM) && 
+//         player_command.type == ADD_PLAYER) {
+//         protocol.sendCommand(create_or_join_command);
+//         std::cout << "Room id created: " << protocol.receiveRoomId() << std::endl;
+
+//         send_thread->start();
+//         receive_thread->start();
+
+//         queue_comandos.push(player_command);
+//         std::shared_ptr<GameState> gs = NULL;
+//         bool leave = false;
+//         while (!leave && !protocol.isFinished()) {
+//             game_states.try_pop(gs);
+//             if (gs) {
+//                 if (!gs->entities.empty()) {
+//                     leave = true;
+//                 }
+//             }
+//         }
+//         if (gs) 
+//             graphics.run(gs, queue_comandos, game_states);
+//     }
+
 void Client::run() {
     std::string line;
     bool started_playing = false;
@@ -24,8 +58,18 @@ void Client::run() {
     command_t create_or_join_command;
     command_t player_command;
     
+    create_or_join_command = commands.cheatInfiniteHitpoints();
+    player_command = commands.cheatInfiniteHitpoints();
+
     this->graphics_qt.run(&commands, &create_or_join_command, &player_command);
 
+
+    if ((create_or_join_command.type != CREATE_ROOM && create_or_join_command.type != JOIN_ROOM) || 
+        player_command.type != ADD_PLAYER) {
+        finished = true;
+        return;
+    }
+    
     protocol.sendCommand(create_or_join_command);
     std::cout << "Room id created: " << protocol.receiveRoomId() << std::endl;
     started_playing = true;
