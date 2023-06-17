76e2d6d41f2806bc55057421468dc77764da7ee5
Pandamos <abrildiazmiguez99@gmail.com>
2023-06-17 16:38:56 -0300

    Agrego barra negra transparente para que la vida y ammo se vean mejor

--- client/visual_bar.cpp
+++ client/visual_bar.cpp
@@ -24,8 +24,14 @@ void VisualBar::update(int32_t new_value) {
 }
 
 void VisualBar::render(int x_origin, int y_origin) {
-    SDL_SetRenderDrawColor(renderer, r_back, g_back, b_back, 255);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 100);
     float total_amount =  max_value;
+    SDL_Rect outer_bar_background = {x_origin-5, y_origin-5, static_cast<int>(total_amount)+10, 10+10}; // position and size of the outer bar
+    SDL_RenderFillRect(renderer, &outer_bar_background);
+
+    SDL_SetRenderDrawColor(renderer, r_back, g_back, b_back, 255);
+    total_amount =  max_value;
     SDL_Rect outer_bar = {x_origin, y_origin, static_cast<int>(total_amount), 10}; // position and size of the outer bar
     SDL_RenderFillRect(renderer, &outer_bar);
 
@@ -34,6 +40,9 @@ void VisualBar::render(int x_origin, int y_origin) {
     float amount_filled = (current_value * total_amount) / max_value; 
     SDL_Rect inner_bar = {outer_bar.x, outer_bar.y, static_cast<int>(amount_filled), outer_bar.h};
     SDL_RenderFillRect(renderer, &inner_bar);
+
+
+
 }
 
 VisualBar::~VisualBar() {}
