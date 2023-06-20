368541d8aa1e8d77294c06627ce9639dca5a77c2
nathencinoza <nencinoza@fi.uba.ar>
2023-06-20 11:15:18 -0300

    Merge

--- client/client_client.cpp
+++ client/client_client.cpp
@@ -43,8 +43,7 @@ void Client::run() {
             int response = protocol.receiveJoinResponse();
             if (response == 1) {
                 std::cout << "Joined room successfully"<< std::endl;
-                create_or_join_command.game_mode = protocol.receiveGameMode();
-            }    
+                create_or_join_command.game_mode = protocol.receiveGameMode()us            }    
             if (response == 0) {
                 std::cout << "Join room failed. Try again." << std::endl;
                 create_or_join_command = commands.cheatInfiniteHitpoints();
