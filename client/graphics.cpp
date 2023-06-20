c4f52e0b93832661279c96e5589c11a1249ef1b3
Pandamos <abrildiazmiguez99@gmail.com>
2023-06-20 08:39:36 -0300

    Bug encontrado: cuando user se une a la partida, no recibe del server el game mode Pero se requiere para levantar la musica correcta Por lo que se lee basura, que no esta en el enum de GameMode Y seg fault
    Subo codigo con parte problematica comentada (para evitar seg faults)
    y comentario en client_client de como se deberia resolver.
    Basicamente, se deberia recibir del servirdor por protocolo el game mode del juego al que
    el user se esta uniendo

--- client/graphics.cpp
+++ client/graphics.cpp
@@ -31,7 +31,9 @@ void Graphics::run(std::shared_ptr<GameState> gs, GameMode game_mode, Queue<comm
         }
 
         AudioHolder audio_holder;
-        //Mix_Music* music = audio_holder.find_music(game_mode);
+        std::cout << "game mode es " << game_mode << std::endl;
+        Mix_Music* music = audio_holder.find_music(game_mode);
+        std::cout << "music es " << music << std::endl;
         // if (Mix_PlayMusic(music, -1) == -1) { //REVISAR
         //     // Error handling: Failed to play the music
         //     throw std::runtime_error("Failed to play music: " + std::string(Mix_GetError()));
