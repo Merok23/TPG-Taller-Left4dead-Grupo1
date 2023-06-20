25da9e9649eead071e416745324f91b82b93c699
nathencinoza <nencinoza@fi.uba.ar>
2023-06-20 15:23:59 -0300

    Updates

--- client/receive_thread.cpp
+++ client/receive_thread.cpp
@@ -18,19 +18,6 @@ void ReceiveThread::run() {
                 finished = true;
                 break;
             }   
-            if (game_state && game_state->game_over) {
-                std::cout<< "ENTRO ACA" << std::endl;
-                std::cout << "GAME OVER" << std::endl;
-                std::pair<uint8_t, uint32_t> infected_killed = game_state->getInfectedKilled();
-                std::cout << infected_killed.second << " infected killed" << std::endl;
-                std::cout << "Ranking: " << (int) infected_killed.first << std::endl;
-                std::pair<uint8_t, uint32_t> ammo_used = game_state->getAmmoUsed();
-                std::cout << ammo_used.second << " ammo used" << std::endl;
-                std::cout << "Ranking: " << (int) ammo_used.first << std::endl;
-                std::pair<uint8_t, uint32_t> game_time = game_state->getGameLoopTime();
-                std::cout << game_time.second << " game loop time" << std::endl;
-                std::cout << "Ranking: " << (int) game_time.first << std::endl;
-            }
             if (game_state) {
                 //game_state->print();
                 game_states.push(std::move(game_state)); 
